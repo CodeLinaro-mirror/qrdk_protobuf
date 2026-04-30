sailfishos-mirror/protobuf

Add an `enforce_proto_limits` feature which will be implemented in Edition 2026.
PiperOrigin-RevId: 892346002

src/google/protobuf/compiler/java/java_features.pb.h
@@ -89,7 +89,7 @@ internal::EnumTraitsT<::pb::JavaFeatures_Utf8Validation_internal_data_>
 namespace internal {
 template <>
 inline ::absl::string_view GetFeatureSetDefaultsData<::pb::JavaFeatures>() {
-  static constexpr char kDefaults[] = "CicYhAciA8o+ACodCAEQAhgCIAMoATACOAJAAco+CggBEAEYACABKAMKJxjnByIDyj4AKh0IAhABGAEgAigBMAE4AkAByj4KCAAQARgAIAEoAwonGOgHIhMIARABGAEgAigBMAHKPgQIABABKg04AkAByj4GGAAgASgDCicY6QciGwgBEAEYASACKAEwATgBQALKPggIABABGAAoASoFyj4CIAAKJxiPTiIbCAEQARgBIAIoATABOANAAso+CAgAEAEYACgBKgXKPgIgACDmByjpBw==";
+  static constexpr char kDefaults[] = "CikYhAciA8o+ACofCAEQAhgCIAMoATACOAJAAUgByj4KCAEQARgAIAEoAwopGOcHIgPKPgAqHwgCEAEYASACKAEwATgCQAFIAco+CggAEAEYACABKAMKKRjoByITCAEQARgBIAIoATAByj4ECAAQASoPOAJAAUgByj4GGAAgASgDCikY6QciGwgBEAEYASACKAEwATgBQALKPggIABABGAAoASoHSAHKPgIgAAopGI9OIh0IARABGAEgAigBMAE4A0ACSALKPggIABABGAAoASoFyj4CIAAg5gco6Qc=";
   return kDefaults;
 }
 }  // namespace internal

CodeLinaro-mirror/qrdk_protobuf

src/google/protobuf/compiler/java/java_features.pb.h
@@ -89,7 +89,7 @@ internal::EnumTraitsT<::pb::JavaFeatures_Utf8Validation_internal_data_>
 namespace internal {
 template <>
 inline ::absl::string_view GetFeatureSetDefaultsData<::pb::JavaFeatures>() {
-  static constexpr char kDefaults[] = "CicYhAciA8o+ACodCAEQAhgCIAMoATACOAJAAco+CggBEAEYACABKAMKJxjnByIDyj4AKh0IAhABGAEgAigBMAE4AkAByj4KCAAQARgAIAEoAwonGOgHIhMIARABGAEgAigBMAHKPgQIABABKg04AkAByj4GGAAgASgDCicY6QciGwgBEAEYASACKAEwATgBQALKPggIABABGAAoASoFyj4CIAAKJxjqByIbCAEQARgBIAIoATABOANAAso+CAgAEAEYACgBKgXKPgIgACDmByjqBw==";
+  static constexpr char kDefaults[] = "CikYhAciA8o+ACofCAEQAhgCIAMoATACOAJAAUgByj4KCAEQARgAIAEoAwopGOcHIgPKPgAqHwgCEAEYASACKAEwATgCQAFIAco+CggAEAEYACABKAMKKRjoByITCAEQARgBIAIoATAByj4ECAAQASoPOAJAAUgByj4GGAAgASgDCikY6QciGwgBEAEYASACKAEwATgBQALKPggIABABGAAoASoHSAHKPgIgAAopGOoHIh0IARABGAEgAigBMAE4A0ACSALKPggIABABGAAoASoFyj4CIAAg5gco6gc=";
   return kDefaults;
 }
 }  // namespace internal